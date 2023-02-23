be091b3e37c4cbdab4cb77258dc3b50cd9794f00
Jihee Hong <486480@student.saxion.nl>
2023-02-23 19:48:28 +0100

    Update proxy in netlify.toml v2

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,10 +1,10 @@
 export const USER_SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'https://travel-shop.onrender.com' + '/api/user'
+    ? '/cors-proxy/https://travel-shop.onrender.com' + '/api/user'
     : 'http://localhost:5000' + '/api/user';
 export const PRODUCT_SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'https://travel-shop.onrender.com' + '/api/product'
+    ? '/cors-proxy/https://travel-shop.onrender.com' + '/api/product'
     : 'http://localhost:5000' + '/api/product';
 // export const USER_SERVER = '/api/user';
 // export const PRODUCT_SERVER = '/api/product';
