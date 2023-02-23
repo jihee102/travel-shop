74b4af62f0db70c32cff74ff3063d4058ffa2cc7
Jihee Hong <486480@student.saxion.nl>
2023-02-23 14:05:33 +0100

    Update backend_url

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,2 +1,3 @@
-export const USER_SERVER = "/api/user";
-export const PRODUCT_SERVER ="/api/product";
+const BACKEND_URL = 'https://travel-shop.onrender.com';
+export const USER_SERVER = BACKEND_URL + '/api/user';
+export const PRODUCT_SERVER = BACKEND_URL + '/api/product';
