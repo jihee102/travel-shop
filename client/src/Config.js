4c3b4b5773f03ef7ff1c6fb337e7d91967311936
Jihee Hong <486480@student.saxion.nl>
2023-02-23 14:54:30 +0100

    Add netlify proxy v3

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,8 +1,8 @@
 export const USER_SERVER =
   process.env.NODE_ENV === 'production'
     ? 'http://localhost:5000'
-    : process.env.BACKEND_URL + '/api/user';
+    : 'https://travel-shop.onrender.com' + '/api/user';
 export const PRODUCT_SERVER =
   process.env.NODE_ENV === 'production'
     ? 'http://localhost:5000'
-    : process.env.BACKEND_URL + '/api/product';
+    : 'https://travel-shop.onrender.com' + '/api/product';
